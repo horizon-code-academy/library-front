d9c378d0677b8a589cece3d3799543cb59b8a521
Malek Boubakri <malekbouba@gmail.com>
2020-10-10 15:46:57 +0100

    home page links

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Row, Col, Card } from "react-bootstrap";
 
 export default function HomePage() {
@@ -8,16 +9,24 @@ export default function HomePage() {
         <h1>Library</h1>
       </Col>
       <Col md={6}>
-        <Card bg="success">
-          <Card.Img variant="top" src="/img/students.jpeg"></Card.Img>
-          <Card.Body>Students</Card.Body>
-        </Card>
+        <Link to="/students">
+          <Card bg="success">
+            <Card.Img variant="top" src="/img/students.jpeg"></Card.Img>
+            <Card.Body>
+              <h1>Students</h1>
+            </Card.Body>
+          </Card>
+        </Link>
       </Col>
       <Col md={6}>
-        <Card bg="info">
-          <Card.Img variant="top" src="/img/books.jpeg"></Card.Img>
-          <Card.Body>Books</Card.Body>
-        </Card>
+        <Link to="/books">
+          <Card bg="info">
+            <Card.Img variant="top" src="/img/books.jpeg"></Card.Img>
+            <Card.Body>
+              <h1>Books</h1>
+            </Card.Body>
+          </Card>
+        </Link>
       </Col>
     </Row>
   );
